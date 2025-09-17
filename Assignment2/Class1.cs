e58700e8e02d47c4fe750b2dacb3d885dfd3eeb1
Vijju-1704 <vijayrakeshanugula@gmail.com>
2025-09-17 18:42:07 +0530

    Add project files.

---
+++ Assignment2/Class1.cs
@@ -0,0 +1,50 @@
+﻿namespace Assignment2
+{
+    public class Class1
+    {
+        public void FindMaxProduct(string number)
+        {
+            if (number.Length < 4)
+            {
+                Console.WriteLine("Input must have at least 4 digits.");
+                return;
+            }
+
+            int maxProduct = 0;
+            string maxDigits = "";
+
+            for (int i = 0; i <= number.Length - 4; i++)
+            {
+                char c1 = number[i];
+                char c2 = number[i + 1];
+                char c3 = number[i + 2];
+                char c4 = number[i + 3];
+
+                if (char.IsDigit(c1) && char.IsDigit(c2) && char.IsDigit(c3) && char.IsDigit(c4))
+                {
+                    int d1 = c1 - '0';
+                    int d2 = c2 - '0';
+                    int d3 = c3 - '0';
+                    int d4 = c4 - '0';
+
+                    int product = d1 * d2 * d3 * d4;
+
+                    if (product > maxProduct)
+                    {
+                        maxProduct = product;
+                        maxDigits = $"{d1}*{d2}*{d3}*{d4}";
+                    }
+                }
+            }
+
+            if (!string.IsNullOrEmpty(maxDigits))
+            {
+                Console.WriteLine($"{maxDigits} = {maxProduct}");
+            }
+            else
+            {
+                Console.WriteLine("No valid 4-digit product found.");
+            }
+        }
+    }
+}
---
+++ Assignment2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Assignment2
+{
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Class1 c1=new Class1();
+
+                while (true)
+                {
+                    Console.Write("\nEnter a number (or press 'q' to quit): ");
+                    string input = Console.ReadLine();
+
+                    if (input.Equals("q", StringComparison.OrdinalIgnoreCase))
+                    {
+                        Console.WriteLine("Exiting...");
+                        break;
+                    }
+
+                    c1.FindMaxProduct(input);
+                }
+            }
+        }
+    
+
+}
